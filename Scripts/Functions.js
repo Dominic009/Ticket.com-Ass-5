300ed7418b23185e782e8dc634b93373123ffd7c
Dominic009 <dpg11000@gmail.com>
2024-02-18 18:41:24 +0600

    started js

--- Scripts/Functions.js
+++ Scripts/Functions.js
@@ -1,11 +1,15 @@
 
 
 function selectedButton(elementClass){
-    const divs = document.querySelectorAll('.selected');
+    const divs = document.querySelectorAll(elementClass);
 
 for (let i = 0; i < divs.length; i++){
     divs[i].addEventListener('click', function(){
         this.style.backgroundColor = '#1DD100';
+        
+        let playerPressed = parseInt(this.innerText)
+        const userPress = playerPressed + 1;
+        console.log(typeof userPress, userPress)
     })
  }
 }
