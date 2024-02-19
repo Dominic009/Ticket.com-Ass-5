0e03b1b17101f1f372b8b0d0e65355c7edfa1035
Dominic009 <dpg11000@gmail.com>
2024-02-19 12:56:35 +0600

    phone number validation

--- Scripts/Functions.js
+++ Scripts/Functions.js
@@ -26,16 +26,16 @@ for (let i = 0; i < seats.length; i++){
     
     seat.addEventListener('click', function selectSeat(){
 
-
-
         let countOfSeats = parseInt(totalSeats.innerText) || 0;
 
         if(countOfSeats < 4){
             seat.style.backgroundColor = '#1DD100';
             countOfSeats++;
             totalSeats.innerText = countOfSeats;
-        }else{
-          return seat.removeEventListener('click', selectSeat())
+            getQuery(seat, 'p');
+        }
+        else{
+          return seat.removeEventListener('click', selectSeat);
         }
         
         const id = document.getElementById('Available-seats');
@@ -43,7 +43,10 @@ for (let i = 0; i < seats.length; i++){
         availableSeats--;
 
         id.innerText = availableSeats;
-;
+
+
+        
+         
 
     })
 }
@@ -61,10 +64,56 @@ function setTextValue(elementid, value){
     element.innerText = value;
 }
 
+function getClassInnerText(elementTag){
+    const element = seat.querySelector(elementTag).innerText;
+    console.log(element)
+}
 
 
 
 
+// setTextValue('seat-number', "Select Seat")
 
 seatSelection();
 setTextValue('total-seats', 0);
+
+
+
+
+function getQuery(element, elementTag){
+   const get = element.querySelector(elementTag).innerText;
+   console.log(get)
+
+           
+   const getSeatNumber = document.getElementById('seat-number');
+   const text = getSeatNumber.innerText;
+   console.log(text)
+   text.innerText = get;
+}
+
+
+
+//Phone number validation to make next button work
+
+const inputNumber = document.getElementById('phone-number');
+const button = document.getElementById('next-button');
+
+function validateNumber(number){
+    let phoneRegex = /^\d{11}$/;
+
+
+
+    inputNumber.addEventListener('input', function(){
+        let phoneNumber = inputNumber.value;
+        const isValidNumber = validateNumber(number);
+
+        if(isValidNumber){
+            button.disabled = false;
+        }else{
+            button.disabled = true;
+        }
+
+    })
+}
+
+validateNumber(inputNumber)
